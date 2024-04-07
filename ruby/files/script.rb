7facfecb125630257bcbfbbcffec394f5bcca247
wilhelm43 <105068971+WWX-Team@users.noreply.github.com>
2024-04-07 19:16:26 +0200

    Update script.rb
    [màj]

--- ruby/files/script.rb
+++ ruby/files/script.rb
@@ -7,7 +7,7 @@ class Imw
     attr_accessor :img, :xsz, :ysz, :res
 
     def initialize(xsize = 0, ysize = 0)
-        @img = Array.new(ysize) {Array.new (xsize){Color.new(cn = 'rgba', value = [0, 0, 0, 0])}}
+        @img = Array.new(ysize) {Array.new (xsize){Color.new()}}
         @xsz = xsize
         @ysz = ysize
         @res = 1
@@ -15,21 +15,36 @@ class Imw
 
     def doc
         puts ""
-        puts "–––––––––+––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+"
-        puts " Class   |                         IMW class (Imw)                          |"
-        puts "         | –– Symbols ––                                                    |"
-        puts "         | .img -> image array                                              |"
-        puts "         | .xsz -> image width                                              |"
-        puts "         | .ysz -> image height                                             |"
-        puts "         | .res -> image resolution                                         |"
-        puts "         | –– Methods ––                                                    |"
-        puts "         | .new(xsize : int, ysize : int)  -> create a new imw              |"
-        puts "         | .crop(xsize : int, ysize : int) -> crop an imw                   |"
-        puts "         | .pack()                         -> pack an imw into its savecode |"
-        puts "         | .unpack(code : imw.pack())      -> update an imw with a savecode |"  
-        puts "–––––––––+––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+"
+        puts "–––––––––+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+"
+        puts " Class   |                         IMW class (Imw)                                 |"
+        puts "         | –– Symbols ––                                                           |"
+        puts "         | .img                                   -> image array                   |"
+        puts "         | .xsz                                   -> image width                   |"
+        puts "         | .ysz                                   -> image height                  |"
+        puts "         | .res                                   -> image resolution              |"
+        puts "         | –– Methods ––                                                           |"
+        puts "         | .new(xsize : int, ysize : int)         -> create a new imw              |"
+        puts "         | .crop(xsize : int, ysize : int)        -> crop an imw                   |"
+        puts "         | .pack()                                -> pack an imw into its savecode |"
+        puts "         | .unpack(code : imw.pack())             -> update an imw with a savecode |"  
+        puts "         | .edit(x : int, y : int, color : Color) -> edit the a pixel's color      |"
+        puts "         | –– Converts ––                                                          |"
+        puts "         | to_s, to_a, to_i                                                        |"
+        puts "–––––––––+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+"
         puts ""
         return self
+    end
+
+    def to_s
+        return self.pack
+    end
+
+    def to_a
+        return self.img
+    end
+
+    def to_i
+        return self.xsz * self.ysz
     end
 
     def crop(xsize = self.xsz, ysize = self.ysz)
@@ -45,10 +60,10 @@ class Imw
         ysize.times { |index|
                         __new_image.append([])
                         xsize.times { |pixel|
-                                        if (index +1) <= __old_y and (pixel +1) <= __old_x
+                                        if (index +1) <= __old_y and (pixel +1) <= __old_x and (pixel +1) < __new_image[index].length
                                                 __new_image[index].append(self.img[index][pixel])
                                         else
-                                            __new_image[index].append(Color.new(cn = 'rgba', value = [0, 0, 0, 0]))
+                                            __new_image[index].append(Color.new())
                                         end
                                     }
                     }
@@ -59,17 +74,13 @@ class Imw
         return self
     end
 
-    def to_str()
-        return self.pack
-    end
-
-    def pack()
+    def pack
         packed =  ""
         packed += self.res.to_s + "\\"
         packed += self.ysz.to_s + "@"
         self.ysz.times  { |index|
                             self.xsz.times  { |pixel|
-                                                packed << self.img[index][pixel].convert(to = 'dec').cl.to_s + "/"
+                                                packed << self.img[index][pixel].to_i.to_s + "/"
                                             }
                             packed << "#/"
                         }
@@ -117,8 +128,8 @@ class Imw
                     __lenX = 0
                 else
                     __lenX += 1
-                    __color = Color.new(cn = 'dec', value = __data.to_i)
-                    __color.convert(to = 'rgba')
+                    __color = Color.new()
+                    __color.edit(rgba(__data.to_i))
                     __line.append(__color)
                 end
                 __data = ''
@@ -127,80 +138,93 @@ class Imw
             end
             idx += 1
         end
-        # Croping Image
-        __img.each_with_index   { |el, line|
-                        if __img[line].length != __img_xsz
-                            __line_len = __img[line].length.to_f
-                            __img[line].each    { |line|
-                                                    ((__img_xsz - __line_len) / 2 - 0.4).round.times  { 
-                                                                                                        __img[line].insert(0, Color.new(cn = 'rgba', value = [0, 0, 0, 0]))
-                                                                                                    }
-                                                    ((__img_xsz - __line_len) / 2 + 0.4).round.times  {
-                                                                                                        __img[line].append(Color.new(cn = 'rgba', value = [0, 0, 0, 0]))
-                                                                                                    }
-                                                }
-                        end
-                                }
         # Building Image
         self.img = __img
         self.ysz = __img_ysz
         self.xsz = __img_xsz
         self.res = __img_res
+        return self.crop()
+    end
+
+    def edit(x = 0, y = 0, color = Color.new())
+        if not (x < self.xsz and y < self.ysz) then return self end
+        if not color.instance_of?(Color)           then return self end
+        self.img[x][y] = color
         return self
     end
+
 end
 
 class Color
 
-    attr_accessor :cn, :cl, :types
+    attr_accessor :cl
 
-    def initialize(cn = 'dec', value = 0)
-        @cl = value
-        @cn = cn
-        @types = ['rgba', 'dec']
+    def initialize(value = [0, 0, 0, 255])
+        if self.is_valid?(value) then asvalue = value
+        else                          asvalue = [0, 0, 0, 255] end
+        @cl = asvalue
     end
 
     def doc
         puts ""
-        puts "–––––––––+––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+"
-        puts " Class   |                            Color class                           |"
-        puts "         | –– Symbols ––                                                    |"
-        puts "         | .cn    -> image array                                            |"
-        puts "         | .cl    -> image width                                            |"
-        puts "         | .types -> image height                                           |"
-        puts "         | –– Methods ––                                                    |"
-        puts "         | .new(cn : Color.types, value : custom)  -> create a new object   |"
-        puts "         | .convert(to : Color.types) -> convert a color to an other format |"
-        puts "–––––––––+––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+"
+        puts "–––––––––+––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+"
+        puts " Class   |                            Color class                         |"
+        puts "         | –– Symbols ––                                                  |"
+        puts "         | .cl                          -> rgba array                     |"
+        puts "         | –– Methods ––                                                  |"
+        puts "         | .new(value   : Array(0, 4))  -> create a new object            |"
+        puts "         | .decimal                     -> output color int code          |"
+        puts "         | .rgba(from)                  -> output rgba tab from int code  |"
+        puts "         | .edit(array)                 -> update obj with array          |"
+        puts "         | .channel(which, value)       -> update rgba channel with value |"
+        puts "         | –– Converts ––                                                 |"
+        puts "         | to_a, to_i                                                     |"
+        puts "–––––––––+––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––+"
         puts ""
         return self
     end
 
-    def convert(to = 'rgba')
-        # Asserts
-        if not self.types.include?(to)
-            return self
-        end
-        # Variables
-        __cn = self.cn
-        if __cn == 'rgba'
-            __as = ((((255 - self.cl[3]) * 256 + self.cl[0]) * 256 + self.cl[1]) * 256 + self.cl[2])
-        elsif __cn == 'dec'
-            __as_c = self.cl
-            # Alpha
-            __as_a = (255 - (__as_c / 16777216)).floor()
-            __as_c -= (255 - __as_a) * 16777216
-            # Canals
-            __as_r = (__as_c / 65536).floor()
-            __as_c -= 65536 * __as_r
-            __as_g = (__as_c / 256).floor()
-            __as_c -= 256 * __as_g
-            __as_b = (__as_c).floor()
-            __as = [__as_r, __as_g, __as_b, __as_a]
-        end
-        # Savind Data
-        self.cn = to
-        self.cl = __as
+    def is_valid?(from)
+        if not from.instance_of?(Array) then return false end
+        from.each { |n|
+                        if not n.instance_of?(Integer) then return false end
+                  }
+        return true
+    end
+
+    def to_i
+        return self.decimal
+    end
+
+    def to_a
+        return self.cl
+    end
+
+    def decimal()
+        return (((255 - self.cl[3]) * 256 + self.cl[0]) * 256 + self.cl[1]) * 256 + self.cl[2]
+    end
+
+    def rgba(from)
+        order  = [3,0,1,2]
+        asint  = from
+        asdef  = [0, 0, 0, 255]
+        asrgba = [0, 0, 0, 255]
+        4.times { |idx|
+                    asrgba[order[idx]] = (asdef[order[idx]] - asint / (256 ** (3 - idx))) .floor .abs
+                    asint              = asint - ((asdef[order[idx]] - asrgba[order[idx]]).abs * (256 ** (3 - idx)))
+                }
+        return asrgba
+    end
+
+    def edit(array)
+        if self.is_valid?(array) then self.cl = array end
+        return self
+    end
+
+    def channel(which, value)
+        channels = ['r', 'g', 'b', 'a']
+        if channels.include?(which) and (value >= 0 and value <= 255)
+            self.cl[channels.index(which)] = value end
         return self
     end
 end
